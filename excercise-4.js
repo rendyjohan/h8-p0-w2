f907c590facb19871319a4dc18f1a813e0d2a48e
rendyjohan <rendyjohan@gmail.com>
2019-10-26 21:26:59 +0700

    tugas week2 excercise-4.js

--- excercise-4.js
+++ excercise-4.js
@@ -23,6 +23,30 @@ switch (bulan){
         bulan = 'Mei';
         break;
 
+    case 6:
+        bulan = 'Juni';
+        break;
+ 
+    case 7:
+        bulan = 'Juli';
+        break;
+        
+    case 8:
+        bulan = 'Agustus';
+        break;        
+
+    case 9:
+        bulan = 'September';
+        break;
+
+    case 10:
+        bulan = 'Oktober';
+        break;
+
+    case 11:
+        bulan = 'November';
+        break;
+        
     default:
         bulan = 'Desember';
         break;
